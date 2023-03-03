72c7d2a0f46b47b9391a4812afeaed69c5156f3f
Dhanushkandasamy <dhanushk.20cse@kongu.edu>
2023-03-03 14:55:05 +0530

    Required auth

---
+++ client/src/Utilis/RequiredAuth.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useAuth } from "./Authentication";
+const RequiredAuth = (props) => {
+  const location = useLocation();
+  const Auth = useAuth();
+  if (!Auth.user) {
+    return <Navigate to="/login" state={{ path: location.pathname }} />;
+  } else {
+    return props.children;
+  }
+};
+export default RequiredAuth;
