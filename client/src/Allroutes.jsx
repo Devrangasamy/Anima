e1ccf50b3941b26ed30c8de63bf56c72d1bdf5cf
Ashwinth <ashwinthk.20cse@kongu.edu>
2023-03-08 15:34:39 +0530

    added

--- client/src/Allroutes.jsx
+++ client/src/Allroutes.jsx
@@ -1,18 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-
-// Utils
 import { Authentication } from "./Utilis/Authentication";
-import RequiredAuth from "./Utilis/RequiredAuth";
-
-// Pages
 import { Home } from "./Pages/Home/Home";
-
-// Components
 import Login from "./components/Login/Login";
 import Signup from "./components/sign up page/signup";
 import Profile from "./components/Profile/Profile";
-import Accessories from "./components/Accessories/Accessories";
-
+import RequiredAuth from "./Utilis/RequiredAuth";
+import  Accessories  from "./components/Accessories/Accessories";
 function Allroutes() {
   return (
     <Authentication>
