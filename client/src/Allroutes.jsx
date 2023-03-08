200b2c11a68524b1663d8f22b059718b922e9800
devrb.20cse@kongu.edu <90512454+Devrangasamy@users.noreply.github.com>
2023-03-08 22:42:37 +0530

    docter body

--- client/src/Allroutes.jsx
+++ client/src/Allroutes.jsx
@@ -8,7 +8,7 @@ import Accessories from "./components/Accessories/Accessories";
 import { Forgetpassword } from "./components/ForgetPassword/Forgetpassword";
 import { Product } from "./Pages/Product_Page/Product";
 import Doctor from "./Pages/Doctor/Doctor";
-
+import RequiredAuth from './Utilis/RequiredAuth';
 function Allroutes() {
   return (
     <Authentication>
