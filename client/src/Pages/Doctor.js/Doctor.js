139d05f73d9ca970ad85e479eb0d054cea12bbb6
Dhanushkandasamy <dhanushk.20cse@kongu.edu>
2023-03-08 19:59:45 +0530

    doctor body create"

--- client/src/Pages/Doctor.js/Doctor.js
+++ client/src/Pages/Doctor.js/Doctor.js
@@ -1,41 +1,14 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
-import './Doctor.css';
-import { FaSearch} from "react-icons/fa";
+import { Searchbar } from '../../components/Doctor/Searchbar';
+import { Doctorbody } from '../../components/Doctor/Doctorbody';
 const Doctor = () => {
-    const [location,setlocation]=useState([]);
-    const [specilization,setspecilization]=useState([]);
-    const [experience,setexperience]=useState(0);
-    const search =()=>{
-
-    }
+    
     return (
         <div>
             <Navbar></Navbar>
-            <div className='doctor-header'>
-                <div className='search'>
-                <select onChange={(e)=>setlocation(e.target.value)}>
-                    <option value="">Location</option>
-                    <option value="salem">SALEM</option>
-                    <option value="erode">ERODE</option>
-                    <option value="coimbatore">COIMBATORE</option>
-                </select>
-                </div>
-                <div className='search' onChange={(e)=>setspecilization(e.target.value)}>
-                <select>
-                    <option value="">Specilization</option>
-                    <option value="dog">DOG</option>
-                    <option value="cat">CAT</option>
-                    <option value="fish">FISH</option>
-                </select>
-                </div>
-                <div className='search'>
-                    <input type="number" placeholder="Years Of Experience" onChange={(e)=>setexperience(e.target.value)}></input>
-                </div>
-                <div className='search'>
-                    <button onClick={search}>Search<FaSearch/></button>
-                </div>
-            </div>
+            <Searchbar></Searchbar>
+            <Doctorbody></Doctorbody>
             <div className='doctor-body'>
             </div>
         </div>
