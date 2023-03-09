4be3d069103635a8fc2d228c477d7d96bf240662
Dhanushkandasamy <dhanushk.20cse@kongu.edu>
2023-03-09 13:47:34 +0530

    Doctor

--- client/src/components/Doctor/Doctorbody.js
+++ client/src/components/Doctor/Doctorbody.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+
 // import { FiMail } from "react-icons/fa";
-import './Doctorbody.css'
+import "./Doctorbody.css";
 export const Doctorbody = () => {
   const [datalist, setDatalist] = useState([]);
   useEffect(() => {
@@ -22,65 +23,57 @@ export const Doctorbody = () => {
           <h1 className="doctor-name">{list.name}</h1>
           <p className="doctor-speciailzation">{list.name}</p>
         </div>
-        <div>
 
         <div className="personaldetails-container">
-          <div>
-          <p className="doctor-features">Experience</p>
-          <p>{list.years_of_experience}</p>
-
+          <div className="grid-2 inside-personal">
+            <p className="doctor-features">Experience</p>
+            <p>{list.years_of_experience}</p>
           </div>
-          <div >
 
-          <p className="doctor-features">language</p>
-          <div>
-          {list.languages_spoken.map((language) => (
-              <p>{language}</p>
+          <div className="grid-2 inside-personal">
+            <p className="doctor-features">Language</p>
+            <div className="grid-2">
+              {list.languages_spoken.map((language) => (
+                <p>{language}</p>
               ))}
-              </div>
-        </div>
-        <div>
-          <p classNmae="doctor-features">certifications</p>
-          <div>
+            </div>
+          </div>
 
-          {list.certifications.map((certification) => (
-            
-            <p>{certification}</p>
-            
-            ))}
+          <div className="grid-2 inside-personal">
+            <p className="doctor-features">Certifications</p>
+            <div className="grid-2">
+              {list.certifications.map((certification) => (
+                <p>{certification}</p>
+              ))}
             </div>
-        </div>
-        <div>
-          <p className="heading">certifications</p>
-          <div>
-            
-          {list.services_provided.map((services) => (
-            
-            <p>{services}</p>
-            
-            ))}
+          </div>
+
+          <div className="grid-2 inside-personal">
+            <p className="doctor-features">Services</p>
+            <div className="grid-2">
+              {list.services_provided.map((services) => (
+                <p>{services}</p>
+              ))}
             </div>
+          </div>
         </div>
-        </div>
-          </div>
-        <div className="personaldetails-container">
-        <address>
-          <div>
-            <i class="fa-solid fa-house"></i>
-            <p>{list.clinic_address}</p>
-          </div>
-          <div>
-            <i class="fa-solid fa-phone"></i>
-            <p>{list.phone_number}</p>
-          </div>
-          <div>
-            <p>{list.email}</p>
-          </div>
-        </address>
+
+        <div className="personaldetails-container doc-address">
+          <address className="doc-address">
+            <div>
+              <p>{list.clinic_address}</p>
+            </div>
+            <div>
+              <p>{list.phone_number}</p>
+            </div>
+            <div>
+              <p>{list.email}</p>
+            </div>
+          </address>
         </div>
       </div>
       <div className="doctor-image-container">
-        <img src={list.image} alt="doctor"></img>
+        <img className="doctor-image" src={list.image} alt="doctor"></img>
       </div>
     </div>
   ));
