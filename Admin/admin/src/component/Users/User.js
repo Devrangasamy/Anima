56f5a9ea4660260d3e0f8916af4c9c3875fe4ecc
Ashwinth <ashwinthk.20cse@kongu.edu>
2023-03-09 21:53:18 +0530

    add

--- Admin/admin/src/component/Users/User.js
+++ Admin/admin/src/component/Users/User.js
@@ -1,20 +1,24 @@
 import axios from 'axios'
 import React,{useState,useEffect} from 'react'
-
+import "../Users/user.css"
 const User = () => {
     const[List, setList] = useState([])
     useEffect(() => {
             axios.get('http://localhost:8000/api/auth/')
             .then((response) => {setList(response.data)})
-            .catch((error) => console.log(error))    
+            .catch((error) => console.log(error))
         },[]
     )
-    console.log(List);
+  console.log(List);
   return (
     <div className='user-container'>
-        <div className='user-inner'>
-            
+      {List.map((x,index)=>{
+        return <div key={index} className='user-inner'>
+          <h3 className='User_name'>{x.username}</h3>
+          <img src = {require('../../icons/logo.jpg')} alt="img" className = 'user-icon'></img>
+          <h5 className='user-cus'>Customer</h5>
         </div>
+    })}
     </div>
   )
 }
