a188ec1e1abf317d5a63585bdf7c1068cf92b9a0
Arunkarthi <arunkarthi827@gmail.com>
2023-03-18 11:38:51 +0530

    git desktop

--- client/src/Pages/Doctor/Doctordetails.js
+++ client/src/Pages/Doctor/Doctordetails.js
@@ -1,6 +1,9 @@
 import './Doctordetails.css';
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams} from 'react-router-dom';
+import axios from 'axios'
 const Doctordetails = () => {
     // const [lists, setList] = useState([]);
     const [datalist, setDatalist] = useState([]);
