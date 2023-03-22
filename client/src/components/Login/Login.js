789c571aea89aa6f9bf1f560a9f516082d024ad0
selva <selvagowtham0110@gmail.com>
2023-03-22 10:58:19 +0530

    Updates in the password side

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,11 @@
+import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../Utilis/Authentication";
 import "../sign up page/signup.css";
-import { useLocation } from "react-router-dom";
+import { LoginUsingGoogle } from "./googleLogin";
+
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,6 +14,8 @@ const Login = () => {
   const location = useLocation();
   const auth = useAuth();
   const[noMatch, setNoMatch] = useState(false)
+  // This is for the password visibility
+  const[showPass, setShowPass] = useState(false)
   const submit = async (e) => {
     e.preventDefault();
     const response = await fetch("http://localhost:8000/api/auth/login", {
@@ -40,43 +45,32 @@ const Login = () => {
 
   return (
     <div className="signup-container">
-      <form onSubmit={submit} className="signup-form">
+      <div className="signup-form">
         <div className="center-contents">
           <h2 id="welcome-text-container">Anima</h2>
         </div>
         <div className="center-contents">
           <h2 id="welcome-text-container">Login</h2>
         </div>
-        <lable className="sign-label">
-          UserName or Email
-          <span className="required">*</span>
-        </lable>
-        <input
-          type="email"
-          placeholder="Enter email"
-          className="sigin-input"
-          value={email}
-          onChange={(e) => {setemail(e.target.value); setNoMatch(false)}}
-        />
+        <label className="sign-label">UserName or Email<span className="required">*</span></label>
+        <input type="email" placeholder="Enter email" className="sigin-input-name" value={email} onChange={(e) => {setemail(e.target.value); setNoMatch(false)}} />
         <br></br>
-        <lable className="sign-label">
-          PassWord<span className="required">*</span>
-        </lable>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          className="sigin-input"
-          value={password}
-          onChange={(e) => {setpassword(e.target.value); setNoMatch(false)}}
-        />
+        <label className="sign-label">PassWord<span className="required">*</span></label>
+        <div className="signin-input-password-div-container">
+          <input type={showPass ? "text" : 'password'} placeholder="Enter Password" className="sigin-input" value={password} onChange={(e) => {setpassword(e.target.value); setNoMatch(false)}}></input>
+          <button onClick = {() => {setShowPass(!showPass)}}>{showPass ? <FontAwesomeIcon icon={faEye}/> : <FontAwesomeIcon icon = {faEyeSlash}/>}</button>
+        </div>
         {noMatch && <> 
           <div className = "login-page-mismatch-password center-container"><span>Mismatch in username and password</span></div>
         </>}
         <div className="center-container">
           <button onClick = {() => forgotPassword()} className = 'signupbutton'>Forgot Password</button>
-          <button className="signupbutton" type="submit" id="sign-up-button">Login</button>
+          <button className="signupbutton" id="sign-up-button" onClick={(e) => submit(e)}>Login</button>
         </div>
-      </form>
+      </div>
+      <div>
+        {/* <LoginUsingGoogle/> */}
+      </div>
     </div>
   );
 };
