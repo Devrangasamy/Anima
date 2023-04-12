6c68732cf28045da734e2fe1bd1d52bae551269d
Arunkarthi <arunkarthi827@gmail.com>
2023-04-12 09:47:53 +0530

    "commit"

--- client/src/Allroutes.jsx
+++ client/src/Allroutes.jsx
@@ -11,8 +11,6 @@ import Doctor from "./Pages/Doctor/Doctor";
 import RequiredAuth from "./Utilis/RequiredAuth";
 import { Cart } from "./Pages/Product/Cart";
 import Doctordetails from "./Pages/Doctor/Doctordetails";
-import { UserUpdate } from "./components/Profile/UserUpdate";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
 function Allroutes() {
   return (
     <Authentication>
@@ -30,7 +28,6 @@ function Allroutes() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup></Signup>} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/userupdate" element={<UserUpdate />} />
           <Route path="/forgetpassword" element={<Forgetpassword />} />
 
           <Route path="/products" element={<Product />} />
