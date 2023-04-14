637793e99095d50dfef1b840dad07bcafe768bd4
selva <selvagowtham0110@gmail.com>
2023-04-14 17:23:42 +0530

    "Updates in the all routes for the google sign in"

--- client/src/components/sign up page/SignupUsingGoogle.js
+++ client/src/components/sign up page/SignupUsingGoogle.js
@@ -1,16 +1,17 @@
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../Utilis/Authentication";
 import { useGoogleContext } from "../../Utilis/GoogleAuthendication";
 
 export const SignupUsingGoogle = () => {
-  const googleAuth = useGoogleContext();
   const navigate = useNavigate();
+  const googleAuth = useGoogleContext()
   return (
     <GoogleOAuthProvider clientId="863387233032-dhmer8ecstkorb5q9kg16u4s3j8b958u.apps.googleusercontent.com">
       <GoogleLogin
         onSuccess={(response) => {
-          googleAuth.updateCredential(response.credential);
+          googleAuth.updateCredential(response.credential)
           navigate("/signup/getPassword");
         }}
         onError={(error) => {
