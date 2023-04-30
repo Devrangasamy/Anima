2f0e11bce5d9bea501b6e945ccd5fa1dc808c13b
Arunkarthi <arunkarthi827@gmail.com>
2023-04-30 19:47:59 +0530

    commit

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -7,32 +7,36 @@ import pet4 from "../../Assets/pet4.jpg";
 import pet5 from "../../Assets/pet5.jpg";
 import next from "../../Assets/next.jpg";
 import slide2image from "../../Assets/Slide2home.jpg";
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from "../../components/Navbar/Navbar";
 import Footers from "../../components/Footer/Footers";
 import { Link } from "react-router-dom";
-import axios from "axios"
+import axios from "axios";
 export const Home = () => {
-  const [List,setList]=useState([]);
+  const [List, setList] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:8000/api/product')
-    .then((response) => 
-     {
-
-      setList([...response.data.slice(response.data.length-4,response.data.length)])})
-    .catch((error) => console.log(error))    
-},[]
-)
+    axios
+      .get("http://localhost:8000/api/product")
+      .then((response) => {
+        setList([
+          ...response.data.slice(
+            response.data.length - 4,
+            response.data.length
+          ),
+        ]);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   // console.log(List[0].photos)
-const displayAllData = List.map((x, index) => 
-<div key = {index} className='col-sm-3 col-md pro-con'>
-    <img className='convert-imgs' src = {x.photos} alt = {""}></img><br></br>
-    <div id='product-page-data-container'>
-        <div id='product-name'>{x.productname}</div>
-        <h4 id='price-container'>MRP₹-{x.cost}</h4>
+  const displayAllData = List.map((x, index) => (
+    <div key={index} className="col-sm-3 col-md pro-con">
+      <img className="convert-imgs" src={x.photos} alt={""}></img>
+      <br></br>
+      <div id="product-page-data-container">
+        <div id="product-name">{x.productname}</div>
+        <h4 id="price-container">MRP₹-{x.cost}</h4>
       </div>
     </div>
-)
-
+  ));
 
   return (
     <div className="home">
@@ -61,10 +65,9 @@ const displayAllData = List.map((x, index) =>
             <img className="main-image" src={slide2image} alt="pet5"></img>
           </div>
         </div>
-      <br></br>  <br></br> <br></br> <br></br>
-      
-    
-      <div className="" >
+      </div>
+      <br></br> <br></br> <br></br> <br></br>
+      <div className="">
         <div>
           <h1 className="kind-service d-flex justify-content-center">
             The Kind of service we provide.
@@ -81,14 +84,18 @@ const displayAllData = List.map((x, index) =>
         <br></br>
         <div>
           <div class="row d-flex justify-content-center container-first">
-          <div class="col-sm-6 col-md-4 col-lg-3 main-first" >
+            <div class="col-sm-6 col-md-4 col-lg-3 main-first">
               <div>
                 <i class="fa-solid fa-heart-circle-plus icon-first"></i>
               </div>
               <div>
                 <h3 class="head-firsts">Pet Care</h3>
                 <br></br>
-            <p class="para-first">Pet care websites provide information, resources, and services related to the care of pets, including dogs, cats, birds, fish, and other animals.</p>
+                <p class="para-first">
+                  Pet care websites provide information, resources, and services
+                  related to the care of pets, including dogs, cats, birds,
+                  fish, and other animals.
+                </p>
               </div>
             </div>
 
@@ -99,7 +106,10 @@ const displayAllData = List.map((x, index) =>
               <div>
                 <h3 class="head-first">Pet Gifts</h3>
                 <br></br>
-            <p class="para-first">Toys, treats, beds, and grooming supplies are all great pet gift ideas.</p>
+                <p class="para-first">
+                  Toys, treats, beds, and grooming supplies are all great pet
+                  gift ideas.
+                </p>
               </div>
             </div>
           </div>
@@ -107,23 +117,28 @@ const displayAllData = List.map((x, index) =>
             <div class="col-sm-6 col-md-4 col-lg-3 main-first">
               <div>
                 <i class="fa-sharp fa-solid fa-heart-pulse icon-first"></i>
-
               </div>
               <div>
                 <h3 class="head-firsts">Medi-Care</h3>
                 <br></br>
-            <p class="para-first">Regular veterinary check-ups, vaccinations, and preventive care measures are essential for maintaining your pet's health and well-being.</p>
+                <p class="para-first">
+                  Regular veterinary check-ups, vaccinations, and preventive
+                  care measures are essential for maintaining your pet's health
+                  and well-being.
+                </p>
               </div>
             </div>
             <div class="col-sm-6 col-md-4 col-lg-3 main-first">
               <div>
-
                 <i class="fa-solid fa-house icon-first"></i>
               </div>
               <div>
                 <h3 class="head-first">Home Visit</h3>
                 <br></br>
-            <p class="para-first">Home visit pet care services provide convenient and personalized care for pets in the comfort of their own homes.</p>
+                <p class="para-first">
+                  Home visit pet care services provide convenient and
+                  personalized care for pets in the comfort of their own homes.
+                </p>
               </div>
             </div>
           </div>
@@ -133,14 +148,23 @@ const displayAllData = List.map((x, index) =>
       <br></br>
       <div className="d-flex justify-content-between px-5">
         <h3 class="trending-pro">New Products</h3>
-       <h6><Link to="/products" className="all-pro">Show all new Products</Link></h6> 
+        <h6>
+          <Link to="/products" className="all-pro">
+            Show all new Products
+          </Link>
+        </h6>
       </div>
       <div>
-      <div class="row p-5 m-5 new-products gap-5" >
+        <div class="row p-5 m-5 new-products gap-5">
           {displayAllData}
-        <Link to="/products" className="all-pro" style={{textAlign:"center",color:"black"}}>See More...</Link>
+          <Link
+            to="/products"
+            className="all-pro"
+            style={{ textAlign: "center", color: "black" }}
+          >
+            See More...
+          </Link>
         </div>
-
       </div>
       <Footers></Footers>
     </div>
